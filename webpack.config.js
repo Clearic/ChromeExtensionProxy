48157ecd716c180420ce8799c8eadb68554e64ed
Alexandr <alexandr@fakemail.com>
2018-07-23 13:19:28 +0500

    compile styles into a separated file

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,36 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
 
 module.exports = {
     entry: "./src/script.js",
     output: {
-    	path: path.resolve(__dirname, "build"),
-    	filename: "bundle.js"
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
     },
     module: {
         rules: [
-            { test: /\.css$/, use: [ "style-loader", "css-loader" ] }
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
         ]
     },
     plugins: [
@@ -19,6 +38,9 @@ module.exports = {
             { from: "index.html", to: "index.html" },
             { from: "icons", to: "icons" },
             { from: "manifest.json", to: "manifest.json" }
-        ])
+        ]),
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        })
     ]
 };
